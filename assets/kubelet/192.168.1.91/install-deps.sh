5e7d4b3270c4715307fd96149e5f0fcb5aa9690f
O. Yuanying <yuanying@fraction.jp>
2021-08-15 02:07:39 +0000

    Bump k8s to v1.21

--- assets/kubelet/192.168.1.91/install-deps.sh
+++ assets/kubelet/192.168.1.91/install-deps.sh
@@ -7,11 +7,11 @@ case $(uname -m) in
 esac
 echo $architecture
 
-CNI_VERSION="v0.8.2"
+CNI_VERSION="v0.9.1"
 mkdir -p /opt/cni/bin
 curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-${architecture}-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
 
-RELEASE="v1.20.3"
+RELEASE="v1.21.3"
 
 mkdir -p /opt/bin
 
